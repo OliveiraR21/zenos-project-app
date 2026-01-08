0a6f27b414582650838df3b7d614e16d95514dfd
Renan José de Oliveira <oliveiira2012@gmail.com>
2026-01-08 17:22:14 +0000

    ## Error Type Console Error
    ## Error Message
    `DialogContent` requires a

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -20,6 +20,8 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
+import { SheetTitle } from './ui/sheet';
+import { VisuallyHidden } from './ui/visually-hidden';
 
 export function AppSidebar() {
   const pathname = usePathname();
@@ -52,6 +54,9 @@ export function AppSidebar() {
             Zenos
           </span>
         </div>
+        <VisuallyHidden>
+            <SheetTitle>Navegação Principal</SheetTitle>
+        </VisuallyHidden>
       </SidebarHeader>
       <SidebarContent>
         <div
